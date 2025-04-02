333b81a828c524394d7da96a98710f3b3fb17eea
sbremer05 <utw5es@virginia.edu>
2025-04-01 21:59:29 -0400

    Initial Commit

---
+++ MyAlarm/MyAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlarmApp.swift
+//  MyAlarm
+//
+//  Created by Sean Bremer on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MyAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyAlarmTests/MyAlarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlarmTests.swift
+//  MyAlarmTests
+//
+//  Created by Sean Bremer on 4/1/25.
+//
+
+import Testing
+@testable import MyAlarm
+
+struct MyAlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

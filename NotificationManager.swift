a241e613f54934bd32e9c84efd03d5144d4aca45
sbremer05 <utw5es@virginia.edu>
2025-04-03 15:04:16 -0400

    notificationmanager added

--- NotificationManager.swift
+++ NotificationManager.swift
@@ -5,35 +5,4 @@
 //  Created by Sean Bremer on 4/3/25.
 //
 
-import UserNotifications
-
-class NotificationManager {
-    static let shared = NotificationManager()
-    
-    private init() {}
-
-    func scheduleNotifications(for alarm: Alarm, reminders: [TimeInterval]) {
-        let notificationCenter = UNUserNotificationCenter.current()
-        notificationCenter.removePendingNotificationRequests(withIdentifiers: [alarm.name]) // Remove old notifications
-
-        for reminder in reminders {
-            let notificationTime = alarm.date.addingTimeInterval(-reminder)
-            if notificationTime > Date() { // Schedule only future notifications
-                let content = UNMutableNotificationContent()
-                content.title = "Alarm Reminder"
-                content.body = "\(alarm.name) is due in \(Int(reminder / 60)) minutes!"
-                content.sound = .default
-
-                let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: notificationTime)
-                let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
-
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                notificationCenter.add(request) { error in
-                    if let error = error {
-                        print("Error scheduling notification: \(error)")
-                    }
-                }
-            }
-        }
-    }
-}
+import Foundation
